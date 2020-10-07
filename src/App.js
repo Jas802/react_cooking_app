541721b9a0dc89a8a427c37944a38ef745157dd7
Jas802 <jas.spencer414@gmail.com>
2020-10-07 12:30:40 -0400

    added components and props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import RecipeList from './RecipeList'
 
 function App() {
-  return "Hello World"
+  return (
+    <RecipeList recipes={sampleReciepes} />
+  )
 }
-
+const sampleReciepes = [
+  {
+    id: 1,
+    name: "Plain Chicken",
+    servings: 3,
+    cookTime: "1:45",
+    instructions: "1. Put salt on Chicken\n2. Put chicken in oven\n 3. Eat chicken"
+  },
+  {
+    id: 2,
+    name: "Plain Pork",
+    servings: 5,
+    cookTime: "0:45",
+    instructions: "1. Put paprika on Pork\n2. Put pork in oven\n 3. Eat pork"
+  }
+]
 export default App;
