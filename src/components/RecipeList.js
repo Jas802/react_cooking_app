8e0cffe0f828016a5281b296990bca2b9b15e4cc
Jas802 <jas.spencer414@gmail.com>
2020-10-08 11:39:17 -0400

    added recipe styling

---
+++ src/components/RecipeList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Recipe from './Recipe'
+export default function RecipeList({ recipes }) {
+    return (
+    <div className="recipe-list">    
+        <div>
+            {recipes.map(recipe => { //puts all recipes in array
+                return ( //returns each recipe
+                <Recipe 
+                    key={recipe.id} //assigns each recipe its own key
+                    {...recipe} 
+                />
+                )
+            })}
+        </div>
+        <div className="recipe-list__add-recipe-btn-container">
+            <button className="btn btn--primary">Add Recipe</button>
+        </div>
+    </div>
+    )
+}
